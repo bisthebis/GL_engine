7309c52eaa96b17a6be745fa22a44816c3849c35
bisthebis <bism25@hotmail.com>
2016-04-03 20:04:22 +0200

    Namespace GL for OpenGL wrapping class (atm only Shader)

--- src/Shader.cpp
+++ src/Shader.cpp
@@ -6,129 +6,133 @@
 
 
 
-Shader::Shader(const std::string& vert, const std::string& frag) : vertexSource(vert), fragmentSource(frag)
+namespace GL
 {
-
-}
-
-Shader::Shader(const Shader& src)	: vertexSource(src.vertexSource), fragmentSource(src.fragmentSource)
-{
-	charger();
-}
-
-Shader::Shader(Shader&& src)
-{
-	std::swap(vertexSource, src.vertexSource);
-	std::swap(fragmentSource, src.fragmentSource);
-	ID = src.ID;
-	src.ID = 0; /*afin d'éviter que le programme ne soit détruit */
-}
-
-Shader& Shader::operator= (const Shader& src)
-{
-	vertexSource = src.vertexSource;
-	fragmentSource = src.fragmentSource;
-	charger();
-	return *this;
-}
-
-Shader& Shader::operator= (Shader&& src)
-{
-	std::swap(vertexSource, src.vertexSource);
-	std::swap(fragmentSource, src.fragmentSource);
-	ID = src.ID;
-	src.ID = 0;
-
-	return *this; /*afin d'éviter que le programme ne soit détruit */
-}
-
-Shader::~Shader()
-{
-	if(glIsProgram(ID))
-	glDeleteProgram(ID);
-}
-
-GLuint Shader::compileShader(GLenum type, const std::string& file)
-{
-	GLuint shader = glCreateShader(type);
-	if(!shader)
-		throw MyException("Bad shader Type !");
-
-	std::ifstream fichier;
-	fichier.open(file);
-
-	if(!fichier)
+	Shader::Shader(const std::string& vert, const std::string& frag) : vertexSource(vert), fragmentSource(frag)
 	{
+
+	}
+
+	Shader::Shader(const Shader& src)	: vertexSource(src.vertexSource), fragmentSource(src.fragmentSource)
+	{
+		charger();
+	}
+
+	Shader::Shader(Shader&& src)
+	{
+		std::swap(vertexSource, src.vertexSource);
+		std::swap(fragmentSource, src.fragmentSource);
+		ID = src.ID;
+		src.ID = 0; /*afin d'éviter que le programme ne soit détruit */
+	}
+
+	Shader& Shader::operator= (const Shader& src)
+	{
+		vertexSource = src.vertexSource;
+		fragmentSource = src.fragmentSource;
+		charger();
+		return *this;
+	}
+
+	Shader& Shader::operator= (Shader&& src)
+	{
+		std::swap(vertexSource, src.vertexSource);
+		std::swap(fragmentSource, src.fragmentSource);
+		ID = src.ID;
+		src.ID = 0;
+
+		return *this; /*afin d'éviter que le programme ne soit détruit */
+	}
+
+	Shader::~Shader()
+	{
+		if(glIsProgram(ID))
+		glDeleteProgram(ID);
+	}
+
+	GLuint Shader::compileShader(GLenum type, const std::string& file)
+	{
+		GLuint shader = glCreateShader(type);
+		if(!shader)
+			throw MyException("Bad shader Type !");
+
+		std::ifstream fichier;
+		fichier.open(file);
+
+		if(!fichier)
+		{
+			glDeleteShader(shader);
+			throw MyException(file + " couldn't be opened.");
+		}
+
+		std::string ligne;
+		std::string Source;
+
+
+		while(std::getline(fichier, ligne))
+					Source += ligne + '\n';
+
+		fichier.close();
+
+		const GLchar* code = Source.c_str();
+
+		glShaderSource(shader, 1, &code, 0);
+		glCompileShader(shader);
+
+		GLint erreurCompilation(0);
+		glGetShaderiv(shader, GL_COMPILE_STATUS, &erreurCompilation);
+
+		if(erreurCompilation != GL_TRUE)
+		{
+
+
+		GLint tailleErreur(0);
+		glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &tailleErreur);
+
+		std::string erreur;
+		erreur.resize(tailleErreur+1);
+
+		glGetShaderInfoLog(shader, tailleErreur, &tailleErreur, &erreur[0]);
+					erreur[tailleErreur] = '\0';
+
 		glDeleteShader(shader);
-		throw MyException(file + " couldn't be opened.");
+
+					throw MyException(file + " couldn't be compiled. Error : " + erreur);
+		}
+		return shader;
+
 	}
 
-	std::string ligne;
-	std::string Source;
-
-
-	while(std::getline(fichier, ligne))
-				Source += ligne + '\n';
-
-	fichier.close();
-
-	const GLchar* code = Source.c_str();
-
-	glShaderSource(shader, 1, &code, 0);
-	glCompileShader(shader);
-
-	GLint erreurCompilation(0);
-	glGetShaderiv(shader, GL_COMPILE_STATUS, &erreurCompilation);
-
-	if(erreurCompilation != GL_TRUE)
+	void Shader::charger()
 	{
+		GLuint vertex = compileShader(GL_VERTEX_SHADER, vertexSource);
+		GLuint fragment = compileShader(GL_FRAGMENT_SHADER, fragmentSource);
 
+		ID = glCreateProgram();
 
-	GLint tailleErreur(0);
-	glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &tailleErreur);
+		glAttachShader(ID, vertex);
+		glAttachShader(ID, fragment);
 
-	std::string erreur;
-	erreur.resize(tailleErreur+1);
+		glLinkProgram(ID);
 
-	glGetShaderInfoLog(shader, tailleErreur, &tailleErreur, &erreur[0]);
-				erreur[tailleErreur] = '\0';
+		GLint LinkStatus = 0;
+		glGetProgramiv(ID, GL_LINK_STATUS, &LinkStatus);
 
-	glDeleteShader(shader);
+		if(!LinkStatus) //Error ! Throw an exception
+		{
+			glDeleteShader(vertex);
+			glDeleteShader(fragment); //avoid leaks
 
-				throw MyException(file + " couldn't be compiled. Error : " + erreur);
-	}
-	return shader;
+			glGetProgramiv(ID, GL_INFO_LOG_LENGTH, &LinkStatus);
+			std::string Log;
+			Log.resize(LinkStatus+1);
 
-}
+			glGetProgramInfoLog(ID, LinkStatus, &LinkStatus, &Log[0]);
+			throw MyException(std::string("Link error. Shader input : ") + vertexSource + " Frag input : " + fragmentSource + ". Eror log : " + Log);
+		}
 
-void Shader::charger()
-{
-	GLuint vertex = compileShader(GL_VERTEX_SHADER, vertexSource);
-	GLuint fragment = compileShader(GL_FRAGMENT_SHADER, fragmentSource);
-
-	ID = glCreateProgram();
-
-	glAttachShader(ID, vertex);
-	glAttachShader(ID, fragment);
-
-	glLinkProgram(ID);
-
-	GLint LinkStatus = 0;
-	glGetProgramiv(ID, GL_LINK_STATUS, &LinkStatus);
-
-	if(!LinkStatus) //Error ! Throw an exception
-	{
 		glDeleteShader(vertex);
-		glDeleteShader(fragment); //avoid leaks
-
-		glGetProgramiv(ID, GL_INFO_LOG_LENGTH, &LinkStatus);
-		std::string Log;
-		Log.resize(LinkStatus+1);
-
-		glGetProgramInfoLog(ID, LinkStatus, &LinkStatus, &Log[0]);
-		throw MyException(std::string("Link error. Shader input : ") + vertexSource + " Frag input : " + fragmentSource + ". Eror log : " + Log);
+		glDeleteShader(fragment);
 	}
 
-	glDeleteShader(vertex);
-	glDeleteShader(fragment);
 }
