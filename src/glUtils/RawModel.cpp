657145901fbef0d7d75e6d408931bb60ae6078e1
bisthebis <bism25@hotmail.com>
2016-04-07 17:44:26 +0200

    Moved glUtils implementations from src to src/glUtils

---
+++ src/glUtils/RawModel.cpp
@@ -0,0 +1,55 @@
+#include <GL/glew.h>
+#include <GL/gl.h>
+
+
+#include "glUtils/RawModel.h"
+
+#include "MyException.h"
+#include "easylogging++.h"
+
+namespace glUtils
+{
+
+  RawModel::RawModel() : VBOs(0) {LOG(INFO) << "Creating a RawModel";}
+  RawModel::~RawModel() {this->cleanup();}
+  void RawModel::cleanup()
+  {
+    if(glIsVertexArray(VAO))
+    {
+      LOG(INFO) << "Deleting a VAO : " << VAO;
+      glDeleteVertexArrays(1, &VAO);
+    }
+
+
+    VAO = 0;
+
+    for (GLuint& VBO : VBOs)
+    {
+      if(glIsBuffer(VBO))
+      {
+        LOG(INFO) << "Deleting a VBO : " << VBO;
+        glDeleteBuffers(1, &VBO);
+      }
+
+
+      VBO = 0;
+    }
+  }
+
+  void RawModel::bindVAO() const
+  {
+    glBindVertexArray(VAO);
+  }
+
+  void RawModel::setVAO(GLuint val)
+  {
+    this->VAO = val;
+  }
+
+  void RawModel::pushVBO(GLuint val)
+  {
+    VBOs.push_back(val);
+  }
+
+
+}
---
+++ src/glUtils/Shader.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+#include <fstream>
+
+#include "glUtils/Shader.h"
+
+#include "MyException.h"
+#include "easylogging++.h"
+
+namespace glUtils
+{
+	Shader::Shader(const std::string& vert, const std::string& frag) : vertexSource(vert), fragmentSource(frag)
+	{
+			LOG(INFO) << "Creating a shader. vertexSource : " << vertexSource << " and fragmentSource : " << fragmentSource;
+	}
+
+	Shader::Shader(Shader&& src)
+	{
+		std::swap(vertexSource, src.vertexSource);
+		std::swap(fragmentSource, src.fragmentSource);
+		ID = src.ID;
+		LOG(INFO) << "Move constructing a shader. vertexSource : " << vertexSource << " and fragmentSource : " << fragmentSource << ". ID : " << ID;
+		src.ID = 0; /*afin d'éviter que le programme ne soit détruit */
+	}
+
+	Shader& Shader::operator= (Shader&& src)
+	{
+		std::swap(vertexSource, src.vertexSource);
+		std::swap(fragmentSource, src.fragmentSource);
+		ID = src.ID;
+		src.ID = 0;
+
+		LOG(INFO) << "Move assigning a shader. vertexSource : " << vertexSource << " and fragmentSource : " << fragmentSource << ". ID : " << ID;
+
+
+		return *this; /*afin d'éviter que le programme ne soit détruit */
+	}
+
+	Shader::~Shader()
+	{
+		if(glIsProgram(ID))
+		{
+			glDeleteProgram(ID);
+			LOG(INFO) << "Deleting Shader";
+		}
+	}
+
+	GLuint Shader::compileShader(GLenum type, const std::string& file)
+	{
+		GLuint shader = glCreateShader(type);
+		if(!shader)
+			throw MyException("Bad shader Type !");
+
+		std::ifstream fichier;
+		fichier.open(file);
+
+		if(!fichier)
+		{
+			glDeleteShader(shader);
+			throw MyException(file + " couldn't be opened.");
+		}
+
+		std::string ligne;
+		std::string Source;
+
+
+		while(std::getline(fichier, ligne))
+					Source += ligne + '\n';
+
+		fichier.close();
+
+		const GLchar* code = Source.c_str();
+
+		glShaderSource(shader, 1, &code, 0);
+		glCompileShader(shader);
+
+		GLint erreurCompilation(0);
+		glGetShaderiv(shader, GL_COMPILE_STATUS, &erreurCompilation);
+
+		if(erreurCompilation != GL_TRUE)
+		{
+
+
+		GLint tailleErreur(0);
+		glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &tailleErreur);
+
+		std::string erreur;
+		erreur.resize(tailleErreur+1);
+
+		glGetShaderInfoLog(shader, tailleErreur, &tailleErreur, &erreur[0]);
+					erreur[tailleErreur] = '\0';
+
+		glDeleteShader(shader);
+
+					throw MyException(file + " couldn't be compiled. Error : " + erreur);
+		}
+		return shader;
+
+	}
+
+	void Shader::charger()
+	{
+		GLuint vertex = compileShader(GL_VERTEX_SHADER, vertexSource);
+		GLuint fragment = compileShader(GL_FRAGMENT_SHADER, fragmentSource);
+
+		ID = glCreateProgram();
+
+		glAttachShader(ID, vertex);
+		glAttachShader(ID, fragment);
+
+		glLinkProgram(ID);
+
+		GLint LinkStatus = 0;
+		glGetProgramiv(ID, GL_LINK_STATUS, &LinkStatus);
+
+		if(!LinkStatus) //Error ! Throw an exception
+		{
+			glDeleteShader(vertex);
+			glDeleteShader(fragment); //avoid leaks
+
+			glGetProgramiv(ID, GL_INFO_LOG_LENGTH, &LinkStatus);
+			std::string Log;
+			Log.resize(LinkStatus+1);
+
+			glGetProgramInfoLog(ID, LinkStatus, &LinkStatus, &Log[0]);
+			throw MyException(std::string("Link error. Shader input : ") + vertexSource + " Frag input : " + fragmentSource + ". Eror log : " + Log);
+		}
+
+		glDeleteShader(vertex);
+		glDeleteShader(fragment);
+	}
+
+}
