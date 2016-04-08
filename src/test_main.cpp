0f0d77c1ca7d053f19efc40abfe4e45ed82e93bf
bisthebis <bism25@hotmail.com>
2016-04-08 20:13:42 +0200

    Camera test via Boost

--- src/test_main.cpp
+++ src/test_main.cpp
@@ -2,8 +2,19 @@
 #include <boost/test/included/unit_test.hpp>
 #include <iostream>
 
-BOOST_AUTO_TEST_CASE( Test1 )
+#include "glUtils/Camera.h"
+
+BOOST_AUTO_TEST_CASE( default_constructed_cameras )
 {
-	BOOST_CHECK((2+2) == 4);
-	BOOST_CHECK((2+2) == 5);
+	glm::mat4 expected[2], actual[2];
+
+	expected[0] = glm::lookAt(glm::vec3(3,3,3), glm::vec3(0,0,0), glm::vec3(0,1,0));
+	glUtils::Camera camera0(glm::vec3(3,3,3), glm::vec3(0,0,0), glm::vec3(0,1,0));
+	camera0.lookAt(actual[0]);
+	BOOST_CHECK(actual[0] == expected[0]);
+
+	expected[1] = glm::lookAt(glm::vec3(3,5,3), glm::vec3(37, -50, 6), glm::vec3(0,1,0));
+	glUtils::Camera camera1(glm::vec3(3,5,3), glm::vec3(37, -50, 6), glm::vec3(0,1,0));
+	camera1.lookAt(actual[1]);
+	BOOST_CHECK(actual[1] == expected[1]);
 }
